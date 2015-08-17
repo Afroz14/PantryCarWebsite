033cd79cf66bddc3cc3e0bdfc9173702d551791f
afroz <afroz.alam@proptiger.com>
2015-08-17 22:45:08 +0530

    About us page indian map added

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -4,7 +4,7 @@
 <body>
 @include('header')
 
-<div class="about-us-header">
+<div class="about-us-header bottom-line-wrap ">
     <h3>About us</h3>
     <div class="bottom-line"></div>
 </div>
@@ -39,7 +39,20 @@
              </ul>
           </div>
   </div>
+<div class="col-md-12 text-center bottom-line-wrap ">
+    <h3>We are currently serving at these locations</h3>
+    <div class="bottom-line"></div>
+    <img src="{{  asset('/img/india-map-location.svg') }}" class="svg-map">
+</div>
 
+<div class="col-md-4 col-md-offset-4 pc-rectangle-block mb20 text-center p20">
+        <h3><strong>Haven't found out your station ?</strong></h3>
+        <h5 class="mb20">Use PantryCar feedback to get things working out .</h5>
+        <a href="{{ url('/contact-us') }}" class="pc-btn">
+              Write to us
+            </a>
+</div>
+ 
 @include('footer')
 </body>
 </html>
